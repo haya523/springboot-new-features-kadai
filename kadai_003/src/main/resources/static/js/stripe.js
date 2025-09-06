43a4b21d35e2958ae7318c9cc4bfe8f8904e2466
haya523 <a.1989052396@gmail.com>
2025-09-06 15:46:49 +0900

    整理: kadai_003/samuraitravel/src のみ残すように構成を修正

--- kadai_003/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51RstPnLIqMKO2K6QiHaYqSJTPmEDj1NBygvviIGS7Jo1XjFHxU3InHBDtuLXkbCvfvXfVWZPVtEbX9vzkOeGR3iX00rfZQJHkM');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
